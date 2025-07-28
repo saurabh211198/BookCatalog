c427ad38ff5b6e61d5f88d84bff1c7797bc7a039
saurabh211198 <137987614+saurabh211198@users.noreply.github.com>
2025-07-28 21:02:15 +0530

    Update UserDetailsServiceImpl.java

--- src/main/java/com/example/BookCatalog/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/BookCatalog/service/UserDetailsServiceImpl.java
@@ -4,17 +4,18 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
-    public UserDetails loadUserByUsername(String username){
-        if(username.equals("admin")){
-            return User.withUsername("admin").password("{noop}admin123").roles("ADMIN").build();
-        } else if(username.equals("user")){
-            return User.withUsername("user").password("{noop}user123").roles("USER").build();
+    public UserDetails loadUserByUsername(String username) {
+        if (username.equals("admin")) {
+            return User.withUsername("admin").password("admin123").roles("ADMIN").build();
+        } else if (username.equals("user")) {
+            return User.withUsername("user").password("user123").roles("USER").build();
+        } else {
+            throw new UsernameNotFoundException("user not found");
         }
-        throw new UsernameNotFoundException("user not found");
     }
 }
+
