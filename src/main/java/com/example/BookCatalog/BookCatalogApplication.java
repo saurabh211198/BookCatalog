07f6eb5417b3fd3c627dd66ae031d9f3d7a46bdd
saurabh211198 <137987614+saurabh211198@users.noreply.github.com>
2025-07-28 21:05:38 +0530

    Update BookCatalogApplication.java

--- src/main/java/com/example/BookCatalog/BookCatalogApplication.java
+++ src/main/java/com/example/BookCatalog/BookCatalogApplication.java
@@ -1,13 +1,17 @@
 package com.example.BookCatalog;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@Slf4j
 @SpringBootApplication
+@EnableCaching
 public class BookCatalogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookCatalogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookCatalogApplication.class, args);
+        System.out.println("Application is up and running");
+    }
 
 }
