a1170676f70d7b6f1b890eac6865071a1f30f0d2
saurabh211198 <137987614+saurabh211198@users.noreply.github.com>
2025-07-28 21:01:19 +0530

    Update BookRepository.java

--- src/main/java/com/example/BookCatalog/repository/BookRepository.java
+++ src/main/java/com/example/BookCatalog/repository/BookRepository.java
@@ -4,11 +4,7 @@ import com.example.BookCatalog.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-
-
 public interface BookRepository extends JpaRepository<Book, Long> {
-
-
 
 
 }
