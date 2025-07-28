6dff0266b8318893a292778db374a506b9910beb
saurabh211198 <137987614+saurabh211198@users.noreply.github.com>
2025-07-28 20:58:25 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/BookCatalog/config/SecurityConfig.java
+++ src/main/java/com/example/BookCatalog/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,19 +23,25 @@ import org.springframework.security.web.SecurityFilterChain;
 )
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html").permitAll()
-                .requestMatchers(HttpMethod.GET,"/books/**").hasAnyRole("USER","ADMIN")
-                .requestMatchers(HttpMethod.POST,"/books/**").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.PUT,"/books/**").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.DELETE,"/books/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                ).httpBasic(Customizer.withDefaults()).build();
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                .requestMatchers(HttpMethod.GET, "/books/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "/books/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/books/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/books/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        ).httpBasic(Customizer.withDefaults()).build();
 
     }
+
     @Bean
-    public UserDetailsService userDetailsService(){
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
         return new UserDetailsServiceImpl();
     }
 }
